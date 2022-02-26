f3d6406ee7dc6fe8bddad884495dfcf62e6aac17
pavolhodas <pavolhodas4@gmail.com>
2022-02-26 14:11:14 +0100

    get 3 random answers from db

--- src/main/java/com/example/demo/answer/AnswerContoller.java
+++ src/main/java/com/example/demo/answer/AnswerContoller.java
@@ -11,10 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Controller
 public class AnswerContoller {
+
+    Random random = new Random();
 
     @Autowired
     AnswerService answerService;
@@ -28,15 +31,29 @@ public class AnswerContoller {
         return answerDto;
     }
 
-    @GetMapping("/get/answers")
+    @GetMapping("/get/answers/random")
     @ResponseBody
     public List<AnswerDto> getAnswers(){
+        int correctAns = 0;
+        int incorrectAns = 0;
+
+        List<Answer> answersToSend = new ArrayList<>();
 
         List<Answer> answerList = new ArrayList<>();
         Iterable<Answer> answers = answerService.findAll();
         answers.forEach(answerList::add);
 
+        do {
+            int randomAns = random.nextInt(answerList.size());
+            if (answerList.get(randomAns).isCorrect() && correctAns == 0) {
+                correctAns++;
+                answersToSend.add(answerList.get(randomAns));
+            } else if (!answerList.get(randomAns).isCorrect() && incorrectAns < 2) {
+                incorrectAns++;
+                answersToSend.add(answerList.get(randomAns));
+            }
+        }while(answersToSend.size() < 3);
 
-        return answerList.stream().map(this::convertToDto).collect(Collectors.toList());
+        return answersToSend.stream().map(this::convertToDto).collect(Collectors.toList());
     }
 }
