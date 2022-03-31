63c0ddeed101069d6c7ea7dc0c6823df600a89d4
pavolhodas <pavolhodas4@gmail.com>
2022-03-31 17:27:32 +0200

    Get sent answers of user, setting an answerServiceImpl

--- src/main/java/com/example/demo/answer/AnswerServiceImpl.java
+++ src/main/java/com/example/demo/answer/AnswerServiceImpl.java
@@ -2,8 +2,13 @@ package com.example.demo.answer;
 
 import com.example.demo.answer.Model.AnswerRepository;
 import com.example.demo.answer.Model.AnswerService;
+import com.example.demo.user.User;
+import com.example.demo.user.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,6 +25,8 @@ public class AnswerServiceImpl implements AnswerService {
 
     ModelMapper modelMapper;
 
+    UserService userService;
+
     @Autowired
     public void setAnswerRepository(AnswerRepository answerRepository) {
         this.answerRepository = answerRepository;
@@ -30,10 +37,17 @@ public class AnswerServiceImpl implements AnswerService {
         this.modelMapper = modelMapper;
     }
 
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public List<AnswerDto> getRandom(long id) {
+        User user = this.getCurrentUser();
         int correctAns = 0;
         int incorrectAns = 0;
+        boolean answerWasUsed = false;
 
         List<Answer> answers = answerRepository.getAllAnsById(id);
         List<Answer> answersToSend = new ArrayList<>();
@@ -41,11 +55,31 @@ public class AnswerServiceImpl implements AnswerService {
         do {
             int randomAns = random.nextInt(answers.size());
             if (answers.get(randomAns).isCorrect() && correctAns == 0) {
-                correctAns++;
-                answersToSend.add(answers.get(randomAns));
+                //setting sent ans for user
+                for(Answer answer : user.getAnswerSet() ){
+                    if(answer == answers.get(randomAns)){
+                        answerWasUsed = true;
+                    }
+                }
+                if(!answerWasUsed){
+                    answersToSend.add(answers.get(randomAns));
+                    correctAns++;
+                }
+                //
+
+                answerWasUsed = false;
             } else if (!answers.get(randomAns).isCorrect() && incorrectAns < 2) {
-                incorrectAns++;
-                answersToSend.add(answers.get(randomAns));
+                //setting sent ans for user
+                for(Answer answer : user.getAnswerSet() ){
+                    if(answer == answers.get(randomAns)){
+                        answerWasUsed = true;
+                    }
+                }
+                if(!answerWasUsed){
+                    answersToSend.add(answers.get(randomAns));
+                    incorrectAns++;
+                }
+                //
             }
         }while(answersToSend.size() < 3);
 
@@ -67,5 +101,15 @@ public class AnswerServiceImpl implements AnswerService {
         answerDto.setContent(answer.getContent());
         answerDto.setId(answer.getId());
         return answerDto;
+    }
+
+    private User getCurrentUser() {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        String username = userDetails.getUsername();
+        return this.userService.getUserByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
     }
 }
